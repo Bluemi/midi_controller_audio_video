c75adcc143b25921630833f8e0765b0b8635da65
Bluemi <bruno.schilling@protonmail.ch>
2017-10-18 15:17:00 +0200

    added midi message logging

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
 console.log("hello world");
+
+function onMidiMessage(event)
+{
+	console.log(
+		event.target.name,
+		event.data,
+		event.receivedTime);
+}
+
+function midiSuccess(midi)
+{
+	console.log("Midi Success :D");
+
+	let inputs = midi.inputs;
+	for (var input of inputs.values()) {
+		input.onmidimessage = onMidiMessage;
+	}
+}
+
+function midiFailure(midi)
+{
+	console.log("Midi Failure :(");
+}
+
+if (navigator.requestMIDIAccess) {
+	navigator.requestMIDIAccess().then(midiSuccess, midiFailure);
+}
+else
+{
+	midiFailure();
+}
