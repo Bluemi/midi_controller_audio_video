b7ee71fc7100589f15df09a00f5092bbc1c77152
Bluemi <bruno.schilling@protonmail.ch>
2018-01-06 17:16:17 +0100

    + web_tmp; GUI Prototype

---
+++ web_tmp/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#my-button").click(function() {
+		for (var i = 0; i < 16; i++) {
+			$(".frame-container").append("<div class=\"sample\"></div>")
+		}
+	});
+});
