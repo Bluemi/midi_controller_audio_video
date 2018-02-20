3c2de2b4c3d186043887ed22f7a513b1783f0812
Bluemi <bruno.schilling@protonmail.ch>
2018-02-20 18:27:38 +0100

    implemented remove button

--- web_tmp/jquery.js
+++ web_tmp/jquery.js
@@ -1,11 +1,39 @@
 $(document).ready(function() {
 	$("#add-button").click(function() {
 		for (var i = 0; i < 16; i++) {
-			$("#loop-panel").append("<div class=\"sample\"></div>")
+			$("#loop-panel").append("<div class=\"sample\" id=\"cell-" + yPos + "-" + i + "\"></div>")
+			$("#cell-" + yPos + "-" + i).click(function() {
+				console.log("hey " + $(this).attr('id'))
+				$(this).css({ background: "red"})
+			});
 		}
-		$("#track-panel").append("<div class=\"track-info\"><button id=\"remove-button\" class=\"remove-button\">x</button></div>")
+		var removeButton = $("<button id=\"remove-button\" class=\"remove-button\">x</button>")
+		//removeButton.yPos = yPos;
+		removeButton.data("yPos", yPos)
+		removeButton.click(function() {
+
+			var y = $(this).data("yPos")
+
+			// remove samples
+			$("[id^='cell-" + y + "-']").remove();
+
+			// remove effects
+			$(".effect").filter(function(index, element) { return $(element).data("yPos") == y; }).remove()
+
+			// remove track panel
+			$(this).parent().remove();
+		});
+
+		var trackPanel = $("<div class=\"track-info\"></div>")
+		trackPanel.append(removeButton)
+		$("#track-panel").append(trackPanel)
 		for (var i = 0; i < 3; i++) {
-			$("#effect-panel").append("<div class=\"effect\"></div>")
+			var effect = $("<div class=\"effect\"></div>")
+			effect.data("yPos", yPos)
+			$("#effect-panel").append(effect)
 		}
+		yPos++
 	});
+
+	yPos = 0
 });
