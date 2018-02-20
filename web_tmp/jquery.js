575a4b1d07a76862cf7842204457da8f69ff8691
cap <phandoom8@gmail.com>
2018-02-20 18:28:16 +0100

    improved sample panel with individual colors

--- web_tmp/jquery.js
+++ web_tmp/jquery.js
@@ -4,4 +4,27 @@ $(document).ready(function() {
 			$(".frame-container").append("<div class=\"sample\"></div>")
 		}
     });
+    for (var j = 0; j < 14; j++){
+        // var back = ["#ff0000","blue","gray"];
+        var rand = getRandomLightColor();
+        $(".sample-container .addableSample #"+j).css("background-color", rand);
+	}
 });
+
+/*function getRandomColor()
+{
+    var color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+    while(color.length < 6) {
+        color = "0" + color;
+    }
+    return "#" + color;
+}*/
+
+function getRandomLightColor() {
+    var letters = 'BCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+}
