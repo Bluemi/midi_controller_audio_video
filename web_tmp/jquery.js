d94120d5b2a65bc21ca4cae6ba5a915e28fddda1
Bluemi <bruno.schilling@protonmail.ch>
2018-02-20 15:58:15 +0100

    aligned add button; added remove button; reformated panels

--- web_tmp/jquery.js
+++ web_tmp/jquery.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-	$("#addButton").click(function() {
+	$("#add-button").click(function() {
 		for (var i = 0; i < 16; i++) {
 			$("#loop-panel").append("<div class=\"sample\"></div>")
 		}
-		for (var i = 0; i < 4; i++) {
-			$("#track-panel").append("<div class=\"sample\"></div>")
-		}
-
-		for (var i = 0; i < 4; i++) {
-			$("#effect-panel").append("<div class=\"sample\"></div>")
+		$("#track-panel").append("<div class=\"track-info\"><button id=\"remove-button\" class=\"remove-button\">x</button></div>")
+		for (var i = 0; i < 3; i++) {
+			$("#effect-panel").append("<div class=\"effect\"></div>")
 		}
 	});
 });
