f4676e3f1f278b7c8450afec19e69acf27f0cd64
Bluemi <bruno.schilling@protonmail.ch>
2018-02-20 14:22:18 +0100

    added track and effect panel; added flex-panel class

--- web_tmp/jquery.js
+++ web_tmp/jquery.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
 		for (var i = 0; i < 16; i++) {
 			$("#loop-panel").append("<div class=\"sample\"></div>")
 		}
+		for (var i = 0; i < 4; i++) {
+			$("#track-panel").append("<div class=\"sample\"></div>")
+		}
+
+		for (var i = 0; i < 4; i++) {
+			$("#effect-panel").append("<div class=\"sample\"></div>")
+		}
 	});
 });
