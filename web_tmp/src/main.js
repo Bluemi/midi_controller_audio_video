a305588216f3f0f8ecfee2bab2f1d6a69cfd3f79
Bluemi <bruno.schilling@protonmail.ch>
2018-02-21 12:52:50 +0100

    added multiple samples

--- web_tmp/src/main.js
+++ web_tmp/src/main.js
@@ -10,7 +10,9 @@ function init() {
         // Fix up for prefixing
         window.AudioContext = window.AudioContext||window.webkitAudioContext;
         context = new AudioContext();
-		loadSampleBuffer("./res/Snare.mp3");
+		loadSampleBuffer("./res/Snare.mp3", "snare");
+		loadSampleBuffer("./res/Kick.mp3", "kick");
+		loadSampleBuffer("./res/HiHat_open.mp3", "hihat_open");
 
     }
     catch(e) {
@@ -18,7 +20,7 @@ function init() {
     }
 }
 
-function loadSampleBuffer(url) {
+function loadSampleBuffer(url, name) {
 	let request = new XMLHttpRequest();
 	request.open('GET', url, true);
 	request.responseType = 'arraybuffer';
@@ -26,37 +28,32 @@ function loadSampleBuffer(url) {
 	// Decode asynchronously
 	request.onload = function() {
 		context.decodeAudioData(request.response, function(buffer) {
-			bufferManager.sample = buffer;
+			bufferManager[name] = buffer;
 
 		}, function(e) {alert("error: " + e)});
 	};
 	request.send();
 }	// note: on older systems, may have to use deprecated noteOn(time);
 
-/*loadSampleBuffer("./res/Kick.mp3");*/
-/*loadSampleBuffer("./res/HiHat_open.mp3");*/
 
 
 function snare() {
-	console.log("snare")
 	let source = context.createBufferSource();
-	source.buffer = bufferManager.sample;
+	source.buffer = bufferManager.snare;
 	source.connect(context.destination);
 	source.start(context.currentTime);
 }
 
 function kick() {
-	console.log("snare")
 	let source = context.createBufferSource();
-	source.buffer = bufferManager.sample;
+	source.buffer = bufferManager.kick
 	source.connect(context.destination);
 	source.start(context.currentTime);
 }
 
 function hihat() {
-	console.log("snare")
 	let source = context.createBufferSource();
-	source.buffer = bufferManager.sample;
+	source.buffer = bufferManager.hihat_open;
 	source.connect(context.destination);
 	source.start(context.currentTime);
 }
