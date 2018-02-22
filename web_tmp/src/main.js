4e7da4da4e4f4aa5bd2c578110214d4f0a7d7923
dephiloper <philippboensch@gmail.com>
2018-02-22 13:28:24 +0100

    extracted audio specific code out of main into player.js

--- web_tmp/src/main.js
+++ web_tmp/src/main.js
@@ -1,13 +1,6 @@
 let context;
-let bufferManager = {};
-let activeSampleId = 0;
 let yPos = 0;
-let tracks = {};
-let numberOfTicks = 16;
-let loopVar;
-
-const INTERVAL = 0.2;
-const OFFSET = 0.1;
+let player;
 
 window.addEventListener('load', init, false);
 function init() {
@@ -15,65 +8,14 @@ function init() {
         // Fix up for prefixing
         window.AudioContext = window.AudioContext||window.webkitAudioContext;
         context = new AudioContext();
-		loadSampleBuffer("./res/Snare.mp3", "snare");
-		loadSampleBuffer("./res/Kick.mp3", "kick");
-		loadSampleBuffer("./res/HiHat_open.mp3", "hihat_open");
+	player = new Player(context);
+	player.loadSampleBuffer("./res/Snare.mp3", "snare");
+	player.loadSampleBuffer("./res/Kick.mp3", "kick");
+	player.loadSampleBuffer("./res/HiHat_open.mp3", "hihat_open");
     }
     catch(e) {
         alert('Web Audio API is not supported in this browser');
     }
-}
-
-function loadSampleBuffer(url, name) {
-	let request = new XMLHttpRequest();
-	request.open('GET', url, true);
-	request.responseType = 'arraybuffer';
-
-	// Decode asynchronously
-	request.onload = function() {
-		context.decodeAudioData(request.response, function(buffer) {
-			bufferManager[name] = buffer;
-
-		}, function(e) {alert("error: " + e)});
-	};
-	request.send();
-}	// note: on older systems, may have to use deprecated noteOn(time);
-
-function snare() {
-	let source = context.createBufferSource();
-	source.buffer = bufferManager.snare;
-	source.connect(context.destination);
-	source.start(context.currentTime);
-	activeSampleId = 1;
-	$("#add-button").prop("disabled", false)
-}
-
-function kick() {
-	let source = context.createBufferSource();
-	source.buffer = bufferManager.kick
-	source.connect(context.destination);
-	source.start(context.currentTime);
-	activeSampleId = 2;
-	$("#add-button").prop("disabled", false)
-}
-
-function hihat() {
-	let source = context.createBufferSource();
-	source.buffer = bufferManager.hihat_open;
-	source.connect(context.destination);
-	source.start(context.currentTime);
-	activeSampleId = 3;
-	$("#add-button").prop("disabled", false)
-}
-
-function enableTick(y, x) {
-	console.log(y)
-	console.log(tracks)
-	tracks[y].ticks[x] = !tracks[y].ticks[x];
-}
-
-function removeTrack(y) {
-	delete tracks[y]
 }
 
 function addGui(addButton) {
@@ -90,7 +32,7 @@ function addGui(addButton) {
 			} else {
 				$(this).css({ background: "white"})
 			}
-			enableTick($(this).data("yPos"), $(this).data("xPos"));
+			player.enableTick($(this).data("yPos"), $(this).data("xPos"));
 		});
 	}
 	var removeButton = $("<button id=\"remove-button\" class=\"remove-button\">x</button>")
@@ -99,7 +41,7 @@ function addGui(addButton) {
 
 		var y = $(this).data("yPos")
 
-		removeTrack(y)
+		player.removeTrack(y)
 
 		// remove samples
 		$("[id^='cell-" + y + "-']").remove();
@@ -123,60 +65,10 @@ function addGui(addButton) {
 	addButton.prop("disabled", true)
 }
 
-function idToSample(id) {
-	switch (id) {
-		case 1:
-			return "snare";
-		case 2:
-			return "kick";
-		case 3:
-			return "hihat_open";
-		default:
-			return "";
-
-	}
-}
-
-function addTrack() {
-	let sample = idToSample(activeSampleId)
-	tracks[yPos] = new Track(sample, bufferManager[sample])
-	activeSampleId = 0;
-}
-
-function loop() {
-	play();
-
-	// wait for loop
-	let loopTime = 0;
-	for (let j = 0; j < numberOfTicks; j++) {
-		loopTime += INTERVAL;
-	}
-	loopVar = window.setInterval(play, loopTime * 1000)
-}
-
-function play() {
-	let t = context.currentTime + OFFSET;
-	for (let k in tracks) {
-		track = tracks[k]
-		for (let tick = 0; tick < track.ticks.length; tick++) {
-			let source = context.createBufferSource();
-			source.buffer = track.buffer;
-			source.connect(context.destination);
-			if (track.ticks[tick] > 0) {
-				source.start(t + tick*INTERVAL);
-			}
-		}
-	}
-}
-
-function stop() {
-	clearInterval(loopVar);
-}
-
 // add gui elements
 $(document).ready(function() {
 	$("#add-button").click(function() {
-		addTrack()
+		player.addTrack()
 		addGui($(this));
 	});
 
--- web_tmp/src/track.js
+++ web_tmp/src/track.js
@@ -1,10 +1,12 @@
 class Track {
-    constructor(title, buffer){
-        this.title = title;
-		this.ticks = [];
-		for (let i = 0; i < numberOfTicks; i++) {
-			this.ticks.push(false);
-		}
-        this.buffer = buffer;
-    }
+	constructor(title, buffer) {
+        	this.title = title;
+			this.ticks = [];
+			for (let i = 0; i < Track.numberOfTicks; i++) {
+				this.ticks.push(false);
+			}
+        	this.buffer = buffer;
+    	}
 }
+
+Track.numberOfTicks = 16;
