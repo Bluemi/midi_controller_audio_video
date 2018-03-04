f41b66557f3aba65c934d69bf2f46df1d55ab2a9
cap <phandoom8@gmail.com>
2018-03-04 19:41:17 +0100

    implemented functionality to change effects on the fly by using midi controller

--- src/midicontroller.js
+++ src/midicontroller.js
@@ -1,6 +1,7 @@
 let midiAccess;
 let $currentTrackInfoFocus;
 let trackCount;
+let normalMax = 127;
 
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log(document.getElementById('result'));
@@ -23,7 +24,6 @@ function midiSuccess(midi) {
     console.log('Midi is working!');
 
     midiAccess = midi;
-    console.log(midi);
     let inputs = midi.inputs;
 
     for (let input of inputs.values()) {
@@ -42,47 +42,60 @@ function onMidiMessage(event) {
     let value = event.data[2];
 
     switch (btnID) {
+        case 1:
+            regulateDelaySize(value);
+            break;
+        case 2:
+            regulateDelayValue(value);
+            break;
         case 16:
-            if(value===0) clickNewButton(-1);
+            if (value === 0) clickNewButton(-1);
             break;
         case 17:
-            if(value===0) clickNewButton(1);
+            if (value === 0) clickNewButton(1);
             break;
         case 48:
+            changeFocusedTrackInfo(value);
+            break;
+        case 49:
             regulateVolume(value);
             break;
+        case 50:
+            regulateBiquadFilterFrequency(value);
+            break;
         case 51:
-            changeFocusedTrackInfo(value);
+            regulateReverb(value);
             break;
         default:
             break;
     }
 /*
-        console.log("\n" +
-        "New Event (on Channel: "+channel+")==> Type: "+ cmd +
-        ", Origin: "+btnID +
-        ", Value: "+value);*/
+
+    console.log("\n" +
+        "New Event (on Channel: " + channel + ")==> Type: " + cmd +
+        ", Origin: " + btnID +
+        ", Value: " + value);
+*/
 }
 
 function clickNewButton(direction) {
     let $focused = $(':focus');
-    let currentId = Number($focused.attr("id"))+Number(direction);
-    let $foundButton = $("#sample-container").find(".addableSample").find("#"+currentId);
-    if (!isNaN($foundButton.attr("id"))){
+    let currentId = Number($focused.attr("id")) + Number(direction);
+    let $foundButton = $("#sample-container").find(".addableSample").find("#" + currentId);
+    if (!isNaN($foundButton.attr("id"))) {
         $focused.blur();
         $foundButton.trigger("click").focus();
     }
 }
 
 function regulateVolume(value) {
-    let max = 127;
-    if ($currentTrackInfoFocus != null && $currentTrackInfoFocus.length !== 0){
-        let currentTrackIndex = $currentTrackInfoFocus.data("yPos");
-        let volume = value / max;
-        player.tracks[currentTrackIndex].setVolume(volume);
-        $("#volume-display"+currentTrackIndex).find(".track-info-button-content").text(Math.floor(volume*100));
-        //$("#volume-display"+currentTrackIndex).append("<input type='image' id='speaker' src='../res/images/speaker/speaker.svg'/>");
-    }
+    if (!isFocusedTrackInfoExistent()) return;
+
+    let currentTrackIndex = $currentTrackInfoFocus.data("yPos");
+    let volume = value / normalMax;
+    player.tracks[currentTrackIndex].setVolume(volume);
+    $("#volume-display"+currentTrackIndex).find(".track-info-button-content").text(Math.floor(volume*100));
+    //$("#volume-display"+currentTrackIndex).append("<input type='image' id='speaker' src='../res/images/speaker/speaker.svg'/>");
 }
 
 function changeFocusedTrackInfo(value) {
@@ -98,4 +111,57 @@ function changeFocusedTrackInfo(value) {
         $newTrackInfoToFocus.css("background-color", "red");
         $currentTrackInfoFocus = $newTrackInfoToFocus;
     }
+}
+
+function regulateBiquadFilterFrequency(value) {
+    if (!isFocusedTrackInfoExistent()) return;
+
+    getTrackByYPos().setBiquadFilterFrequency((value / normalMax) * 1000);
+    visualizeEffectByColor(1, value/normalMax);
+}
+
+function regulateReverb(value) {
+    if (!isFocusedTrackInfoExistent()) return;
+
+    getTrackByYPos().setReverbValue((value/normalMax)*0.03);
+    visualizeEffectByColor(2, value/normalMax);
+}
+
+function regulateDelaySize(value) {
+    if (!isFocusedTrackInfoExistent()) return;
+
+    getTrackByYPos().setDelaySize(value/normalMax);
+}
+
+function regulateDelayValue(value) {
+    if (!isFocusedTrackInfoExistent()) return;
+
+    getTrackByYPos().setDelayValue(value/normalMax);
+    visualizeEffectByColor(0, value/normalMax);
+}
+
+function getTrackByYPos() {
+    return player.tracks[$currentTrackInfoFocus.data("yPos")];
+}
+
+function isFocusedTrackInfoExistent() {
+    return $currentTrackInfoFocus != null && $currentTrackInfoFocus.length > 0;
+}
+
+function visualizeEffectByColor(effectIndex, value) {
+    let index = $currentTrackInfoFocus.data("yPos");
+    $('#effect-panel > .effect').each(function () {
+        if ($(this).data("yPos") === index && $(this).data("x") === effectIndex) {
+            $(this).css("background-color", changeCSSColorHSL(180, value*100, 240));
+        }
+    });
+
+}
+
+function changeCSSColorHSL(start, percentage, end) {
+    let x = percentage / 100,
+        y = (end - start) * x,
+        z = y + start;
+
+    return 'hsl('+z+', 100%, 50%)';
 }
